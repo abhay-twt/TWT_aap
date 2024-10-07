16d324219e6c1d12a2393991b1d48d8c84a224aa
VidhyaSubash-Source <ambethsubash04@gmail.com>
2024-10-07 15:25:06 +0530

    Code - Image From Gallery

--- app/src/main/java/com/example/photoapp/CameraActivity.java
+++ app/src/main/java/com/example/photoapp/CameraActivity.java
@@ -2,21 +2,29 @@ package com.example.photoapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class CameraActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> resultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        registerResult();
     }
     public void captureImage(View view) {
         Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -31,5 +39,34 @@ public class CameraActivity extends AppCompatActivity {
             capturedImageView.setImageBitmap(photo);
 
         }
+    }
+
+    public void registerResult(){
+                resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        try{
+                            Uri imageUri = result.getData().getData();
+                            ImageView capturedImageView = findViewById(R.id.cameraImageView);
+                            capturedImageView.setImageURI(imageUri);
+                        }
+                        catch (Exception e)
+                        {
+                            Toast.makeText(CameraActivity.this,"No Image Selected",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+    public void imageFromGallery(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+        resultLauncher.launch(intent);
+    }
+
+    public void moveToNext(View view) {
+        Intent intent = new Intent(CameraActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
