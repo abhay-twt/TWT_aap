ca299a628f3fa6dfefa051d66370da6f3b8d2fc9
VidhyaSubash-Source <ambethsubash04@gmail.com>
2024-10-07 14:29:54 +0530

    Camera Screen

--- app/src/main/java/com/example/photoapp/CameraActivity.java
+++ app/src/main/java/com/example/photoapp/CameraActivity.java
@@ -1,15 +1,35 @@
 package com.example.photoapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 
 public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_camera);
+    }
+    public void captureImage(View view) {
+        Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(open_camera,123);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+            Bitmap photo = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+            ImageView capturedImageView = findViewById(R.id.cameraImageView);
+            capturedImageView.setImageBitmap(photo);
 
+        }
     }
 }
